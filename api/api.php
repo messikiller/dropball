8e8046d507fe8e20b9e04fd2e686ba887a5a7c38
heqiming <heqiming@weitoutiao.com>
2016-05-19 10:24:09 +0800

    init reposity

---
+++ api/api.php
@@ -0,0 +1,20 @@
+<?php
+//move_time 小球运动的时间
+$m_time = $_GET['move_time'];
+//drop_height 小球坠落的初始高度
+$drop_height = $_GET['drop_height'];
+//从开始坠落到碰到地面需要的时间
+$per_time = sqrt(2*$drop_height/10);
+//判读小球正在下落还是弹回
+$t = $m_time % (2*$per_time);
+if( $t< $per_time ){
+    //小球正在下落
+    $r = 0.5*10*$t*$t;
+} else {
+    //小球正在弹回
+    $t=$t-$per_time;
+    $t=$per_time-$t;
+    $r = 0.5*10*$t*$t;
+}
+echo $r;
+?>
